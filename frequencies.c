c5ebea755c2b9eeea69b4707330ad1f9d1c0f2e9
je1rav <je1rav@gmail.com>
2024-10-24 13:18:36 +0900

    Update frequencies.c

--- frequencies.c
+++ frequencies.c
@@ -175,7 +175,7 @@ int32_t TX_freq_check(const uint32_t Frequency)
         case F_LOCK_ALL:
             break;
         default:  
-            if (Frequency >= 14400000 && Frequency < 14600000)
+            if (Frequency >= 5000000 && Frequency < 5400000)
             return 0;
             if (Frequency >= 43000000 && Frequency < 44000000)
             return 0;
